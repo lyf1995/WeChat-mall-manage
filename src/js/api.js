454efb9c67c5f85b1bfe570f07dc8c3367a1702c
lyf1995notebook <506833107@qq.com>
2018-05-11 23:17:31 +0800

    完成查询订单列表的显示

--- src/js/api.js
+++ src/js/api.js
@@ -38,4 +38,6 @@ export const DeleteCommodity = params=>{ return axios.post(`${base}/commodity/de
 //还原商品
 export const ReductionCommodity = params=>{ return axios.post(`${base}/commodity/reductionCommodity`, params).then(res => res.data).catch(error=>error); };
 //查询所有订单
-export const SelectAllOrder = params=>{ return axios.post(`${base}/order/SelectAllOrder`, params).then(res => res.data).catch(error=>error); };
+export const SelectAllOrder = params=>{ return axios.post(`${base}/order/selectAllOrder`, params).then(res => res.data).catch(error=>error); };
+//修改订单状态
+export const ChangeOrderStatus = params =>{ return axios.post(`${base}/order/changeOrderStatus`, params).then(res => res.data).catch(error => error); };
--- src/pages/OrderList.vue
+++ src/pages/OrderList.vue
@@ -8,15 +8,15 @@
   				<el-form-item label="订单状态">
     				<el-select v-model="searchInfo.status" placeholder="订单状态">
     					<el-option label="全部" value="-1"></el-option>
-    					<el-option label="待付款" value="0"></el-option>
+    					<el-option label="未付款" value="0"></el-option>
     					<el-option label="待发货" value="1"></el-option>
     					<el-option label="待收货" value="2"></el-option>
     					<el-option label="已完成" value="3"></el-option>
     					<el-option label="已取消" value="4"></el-option>
     				</el-select>
   				</el-form-item>
-  				<el-form-item label="客户信息">
-    				<el-input v-model="searchInfo.userName" placeholder="客户信息"></el-input>
+  				<el-form-item label="下单客户">
+    				<el-input v-model="searchInfo.phone" placeholder="下单客户"></el-input>
   				</el-form-item>
   				<el-form-item label="下单时间">
   					<el-date-picker v-model="searchInfo.orderTime" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd">
@@ -39,9 +39,13 @@
 			</div>
 			<el-table :data="orderList" border style="width: 100%" v-loading="loading">
 				<el-table-column type="index" label="序号" width="50"></el-table-column>
-				<el-table-column prop="number" label="订单编号"></el-table-column>
-				<el-table-column prop="status" label="订单状态"></el-table-column>
-				<el-table-column prop="userName" label="下单客户"></el-table-column>
+				<el-table-column prop="number" label="订单编号" width="150"></el-table-column>
+				<el-table-column prop="status" label="订单状态" width="80">
+					<template slot-scope="scope">
+						{{scope.row.status|statusFormat}}
+					</template>
+				</el-table-column>
+				<el-table-column prop="phone" label="下单客户" width="120"></el-table-column>
 				<el-table-column label="订单详情" width="300">
 					<template slot-scope="scope">
         				<div class="commodity" v-for="(item, index) in scope.row.productsList" style="padding-bottom:10px;">
@@ -51,8 +55,7 @@
         				</div>
       				</template>
 				</el-table-column>
-				<el-table-column prop="money" label="金额"></el-table-column>
-				<el-table-column label="收货信息">
+				<el-table-column label="收货信息" width="180">
 					<template slot-scope="scope">
         				<el-tooltip placement="right">
 						  	<div slot="content" style="width:300px;">
@@ -67,21 +70,31 @@
 						</el-tooltip>
       				</template>
 				</el-table-column>
-				<el-table-column prop="remarks" label="备注"></el-table-column>
-				<el-table-column prop="orderTime" label="下单时间"></el-table-column>
+				<el-table-column label="金额" width="80">
+					<template slot-scope="scope">
+						￥{{scope.row.totalAmount|monyFormat}}
+					</template>
+				</el-table-column>
+				<el-table-column prop="remarks" label="备注" width="120"></el-table-column>
+				<el-table-column prop="orderTime" label="下单时间" width="180"></el-table-column>
 				<el-table-column label="操作" width="200" fixed="right" align="center">
       				<template slot-scope="scope">
-        				<el-button size="mini" type="primary">
+        				<el-button size="mini" type="primary" v-if="scope.row.status == 1" @click="sendGoods(scope.row.id)">
         					<i class="iconfont icon-icon6" style="margin-right:5px;"></i>发货
         				</el-button>
       				</template>
 				</el-table-column>
 			</el-table>
+			<div class="pagination_wrap">
+  				<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"  :page-sizes="[15, 30, 100, 200]"
+  				:page-size="searchInfo.pageSize" :current-page="searchInfo.pageIndex" layout="total, sizes, prev, pager, next, jumper" :total="searchInfo.total">
+			    </el-pagination>
+  			</div>
 		</el-card>
 	</div>
 </template>
 <script>
-	import { SelectAllOrder } from '@/js/api'
+	import { SelectAllOrder,ChangeOrderStatus } from '@/js/api'
 	export default {
 		data(){
 			return{
@@ -89,12 +102,37 @@
 				searchInfo: {
 					number: '',
 					status: '-1',
-					userName: '', 
+					phone: '', 
 					orderTime: '',
 					orderStartTime: '',
-					orderEndTime: ''
+					orderEndTime: '',
+					pageSize: 15,
+					pageIndex: 1,
+					total: 0
 				},
 				orderList: []
+			}
+		},
+		filters:{
+			statusFormat(val){
+				if(val == 0){
+					return '未付款'
+				}
+				else if(val == 1){
+					return '待发货'
+				}
+				else if(val == 2){
+					return '待收货'
+				}
+				else if(val == 3){
+					return '已完成'
+				}
+				else if(val == 4){
+					return '已取消'
+				}
+			},
+			monyFormat(val){
+				return parseInt(val).toFixed(2);
 			}
 		},
 		methods:{
@@ -107,6 +145,7 @@
 					let { errMsg, errCode, value, success, extraInfo } = data;
 					if(success){
 						this.orderList = value;
+						this.searchInfo.total = JSON.parse(extraInfo);
 					}
 					else{
 						this.$message({
@@ -114,12 +153,49 @@
 							type: 'error'
 						})
 					}
+					this.loading = false;
 				})
 			},
-			search(){
-
+			handleSizeChange(val){
+				this.searchInfo.pageSize=val;
+				this.getOrderInfoList();
 			},
-
+			handleCurrentChange(val){
+				this.searchInfo.pageIndex=val;
+				this.getOrderInfoList();
+			},
+			search(){
+				this.getOrderInfoList();
+			},
+			changeOrderStatus(id,status){
+				ChangeOrderStatus({id,status}).then(data =>{
+					let { errMsg, errCode, value, success, extraInfo } = data;
+					if(success){
+						this.$message({
+							message:'发货成功',
+							type: 'success'
+						});
+						this.getOrderInfoList();
+					}
+					else{
+						this.$message({
+							message:'发货失败',
+							type: 'error'
+						})
+					}
+				});
+			},
+			sendGoods(id){
+				this.$confirm('是否确定发货?', '提示', {
+		          	confirmButtonText: '确定',
+	         	 	cancelButtonText: '取消',
+		          	type: 'warning'
+		        }).then(() => {
+		        	this.changeOrderStatus(id,2);    	
+		        }).catch(() => {
+		           
+		        });
+			},
 			//导出
 			exportExcel(){
 
@@ -160,5 +236,9 @@
 	.tool_middle p{
 		padding: 5px 0;
 		font-size: 14px;
+	}
+	.pagination_wrap{
+		float:right;
+		padding:20px 0;
 	}
 </style>
